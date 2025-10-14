3a3a89bd184df3b4cccb92b3c330f8e58893eddf
DJIMROM <djimrom@alumno.upv.es@ldsic-vdi10.upvnet.upv.es>
2025-10-14 19:49:23 +0200

    Añadida interfaz genérica List

---
+++ List.h
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+#ifndef LIST_H
+#define LIST_H
+
+template <typename T>
+class List
+{
+	public:
+		virtual void insert(int pos, T e) = 0;
+		virtual void append(T e) = 0;
+		virtual void prepend(T e) = 0;
+		T remove(int pos) = 0;
+		T get(int pos) = 0;
+		int search(T e) = 0;
+		bool empty();
+		int size();
+};
+
+#endif
