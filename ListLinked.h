17ace869e6bf73b38b88c1c337354455843058d9
DJIMROM <djimrom@alumno.upv.es@ldsic-vdi20.upvnet.upv.es>
2025-10-17 15:26:04 +0200

    Actualización 17/0 15:25 CON VIOLACION DE SEGMENTO

--- ListLinked.h
+++ ListLinked.h
@@ -37,15 +37,15 @@ class ListLinked : public List<T>
 			return aux->data; //Devuelvo el valor almacenado en la posición especificada
 		}
 
-		friend std::ostream& operator<<(std::ostream &out, const ListLinked<T>* &list) //Sobrecarga del operador << para imprimir una
+		friend std::ostream& operator<<(std::ostream &out, const ListLinked<T> &list) //Sobrecarga del operador << para imprimir una
 											      //instancia de la lista
 		{
 			out << "List (Node): ";
 			Node<T>* aux = list.first; //Creo un nodo auxiliar para recorrer la lista
-			for(int i = 0; i < list->n; i++)
+			for(int i = 0; i < list.n; i++)
 			{
 				out << aux->data; //Almaceno en out el valor del nodo actual
-				if(i < list->n - 1) out << " -> "; //Si no es la ultima posición pongo una flecha entre medias
+				if(i < list.n - 1) out << " -> "; //Si no es la ultima posición pongo una flecha entre medias
 				aux = aux->next; //Avanzo al siguiente nodo
 			}
 			return out;
@@ -55,7 +55,7 @@ class ListLinked : public List<T>
 		
 		void insert(int pos, T e) override
 		{
-			Node<T>* newNode; //Creo un nuevo nodo para insertar el elemento en la lista
+			Node<T>* newNode = new Node<T>(e); //Creo un nuevo nodo para insertar el elemento en la lista
 			Node<T>* aux = first; //Creo un nodo auxiliar para recorrer la lista
 			for(int i = 0; i < pos; i++) //Recorro la lista con un bucle for
 			{
@@ -63,6 +63,7 @@ class ListLinked : public List<T>
 			}
 			if(pos == 0)
 			{
+				first = newNode;
 				first->next = aux; //Pongo a aux (que es una copia del primer elemento) en segunda posición
 				first->data = e; //Cambio el primer elemento al elemento especificado
 			}
@@ -72,6 +73,7 @@ class ListLinked : public List<T>
 				aux->next = newNode; //Pongo el nuevo nodo detrás del nodo que estaba delante del nodo antiguo
 				newNode->data = e; //Almaceno en el nuevo nodo el elemento especificado
 			}
+			n++;
 		}
 
 		void append(T e) override
@@ -89,7 +91,7 @@ class ListLinked : public List<T>
 			if(pos < 0 || pos > n - 1) throw out_of_range("Posición fuera del rango (0, size() - 1)");
 			Node<T>* aux1 = first; //Creo dos punteros para recorrer la lista
 			Node<T>* aux2;
-			for(int i = 0; i < pos; i++) //Recorro la lista con un bucle for
+			for(int i = 0; i < pos - 1; i++) //Recorro la lista con un bucle for
 			{
 				aux2 = aux1; //aux2 marca la posición de antes del elemento que queremos eliminar
 				aux1 = aux1->next; //aux1 marca la posición del elemento que queremos eliminar
